82ef434393195dd998498353621700327d605031
pmitja <62394071+pmitja@users.noreply.github.com>
2021-10-31 20:41:59 +0100

    Adding feature button functionality
    Update on old code, and adding new feature code

--- js/script.js
+++ js/script.js
@@ -81,23 +81,32 @@ checkFlexGap();
 const dot1 = document.querySelector(".dot-one");
 const dot2 = document.querySelector(".dot-two");
 const dot3 = document.querySelector(".dot-three");
+const dot4 = document.querySelector(".dot-four");
 
 const first = document.querySelector(".first-price");
 const second = document.querySelector(".second-price");
 const third = document.querySelector(".third-price");
+const fourth = document.querySelector(".fourth-price");
 
 dot1.addEventListener("click", () => {
   if (dot2.classList.contains("active")) {
     dot2.classList.remove("active");
     dot1.classList.add("active");
   }
+
   if (dot3.classList.contains("active")) {
     dot3.classList.remove("active");
+    dot1.classList.add("active");
+  }
+
+  if (dot4.classList.contains("active")) {
+    dot4.classList.remove("active");
     dot1.classList.add("active");
   }
   second.classList.add("hide");
   third.classList.add("hide");
   first.classList.remove("hide");
+  fourth.classList.add("hide");
 });
 
 dot2.addEventListener("click", () => {
@@ -105,14 +114,21 @@ dot2.addEventListener("click", () => {
     dot1.classList.remove("active");
     dot2.classList.add("active");
   }
+
   if (dot3.classList.contains("active")) {
     dot3.classList.remove("active");
+    dot2.classList.add("active");
+  }
+
+  if (dot4.classList.contains("active")) {
+    dot4.classList.remove("active");
     dot2.classList.add("active");
   }
 
   second.classList.remove("hide");
   third.classList.add("hide");
   first.classList.add("hide");
+  fourth.classList.add("hide");
 });
 
 dot3.addEventListener("click", () => {
@@ -120,11 +136,88 @@ dot3.addEventListener("click", () => {
     dot1.classList.remove("active");
     dot3.classList.add("active");
   }
+
   if (dot2.classList.contains("active")) {
     dot2.classList.remove("active");
     dot3.classList.add("active");
   }
+
+  if (dot4.classList.contains("active")) {
+    dot4.classList.remove("active");
+    dot3.classList.add("active");
+  }
+
   second.classList.add("hide");
   third.classList.remove("hide");
   first.classList.add("hide");
+  fourth.classList.add("hide");
+});
+
+dot4.addEventListener("click", () => {
+  if (dot1.classList.contains("active")) {
+    dot1.classList.remove("active");
+    dot4.classList.add("active");
+  }
+  if (dot3.classList.contains("active")) {
+    dot3.classList.remove("active");
+    dot4.classList.add("active");
+  }
+
+  if (dot2.classList.contains("active")) {
+    dot2.classList.remove("active");
+    dot4.classList.add("active");
+  }
+  second.classList.add("hide");
+  third.classList.add("hide");
+  first.classList.add("hide");
+  fourth.classList.remove("hide");
+});
+
+///////////////////////////////////////////////////////////
+// Buttons for sections
+
+const praznovanjeSection = document.querySelector("#praznovanje-section");
+const smucanjeSection = document.querySelector("#smucanje-section");
+const plavanjeSection = document.querySelector("#plavanje-section");
+const tenisSection = document.querySelector("#tenis-section");
+
+const praznovanjeBtn = document.querySelector("#btn-praznovanje");
+const smucanjeBtn = document.querySelector("#btn-smucanje");
+const plavanjeBtn = document.querySelector("#btn-plavanje");
+const tenisBtn = document.querySelector("#btn-tenis");
+
+praznovanjeBtn.addEventListener("click", () => {
+  const viewPlace = document.querySelector(".text-box");
+  viewPlace.append(praznovanjeSection);
+  praznovanjeSection.classList.remove("hide");
+  smucanjeSection.classList.add("hide");
+  plavanjeSection.classList.add("hide");
+  tenisSection.classList.add("hide");
+});
+
+smucanjeBtn.addEventListener("click", () => {
+  const viewPlace = document.querySelector(".text-box");
+  viewPlace.append(smucanjeSection);
+  praznovanjeSection.classList.add("hide");
+  smucanjeSection.classList.remove("hide");
+  plavanjeSection.classList.add("hide");
+  tenisSection.classList.add("hide");
+});
+
+plavanjeBtn.addEventListener("click", () => {
+  const viewPlace = document.querySelector(".text-box");
+  viewPlace.append(plavanjeSection);
+  praznovanjeSection.classList.add("hide");
+  smucanjeSection.classList.add("hide");
+  plavanjeSection.classList.remove("hide");
+  tenisSection.classList.add("hide");
+});
+
+tenisBtn.addEventListener("click", () => {
+  const viewPlace = document.querySelector(".text-box");
+  viewPlace.append(tenisSection);
+  praznovanjeSection.classList.add("hide");
+  smucanjeSection.classList.add("hide");
+  plavanjeSection.classList.add("hide");
+  tenisSection.classList.remove("hide");
 });
